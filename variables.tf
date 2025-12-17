083225901b6f0db100fa50a2f9058fa520e83124
annesafrina <93654637+annesafrina@users.noreply.github.com>
2025-12-17 12:46:55 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "tenant_domain" {
+  description = "anneyulianastudentpresident.onmicrosoft.com"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the lab"
+  type        = string
+  default     = "Southeast Asia"
+}
